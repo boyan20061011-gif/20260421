15ba9fc53dd156a5b360e91e2c05ebe6e3539d66
boyan20061011-gif <boyan20061011@gmail.com>
2026-04-21 10:28:39 +0800

    1.01_修改攝影機左右顛倒問題

--- sketch.js
+++ sketch.js
@@ -13,6 +13,12 @@ function draw() {
   let w = width * 0.6;
   let h = height * 0.6;
   
-  // 將影像繪製在畫布中間
-  image(capture, (width - w) / 2, (height - h) / 2, w, h);
+  push();
+  // 將座標原點移至畫布中心
+  translate(width / 2, height / 2);
+  // 水平翻轉座標系 (x 軸乘以 -1)
+  scale(-1, 1);
+  // 在翻轉後的座標系中，將影像繪製在中心 (起點為 -w/2, -h/2)
+  image(capture, -w / 2, -h / 2, w, h);
+  pop();
 }
