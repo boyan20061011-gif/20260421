21f0c599936d1782053fd719754960c2960c5dd9
boyan20061011-gif <boyan20061011@gmail.com>
2026-04-21 10:40:58 +0800

    1.02_產生一張圖片與視訊內容一樣

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let pg;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -9,6 +10,19 @@ function setup() {
 function draw() {
   background('#e7c6ff');
   
+  // 當攝影機準備好且 pg 尚未建立時，根據攝影機尺寸建立繪圖層
+  if (!pg && capture.width > 0) {
+    pg = createGraphics(capture.width, capture.height);
+    
+    // 在 pg 上畫一些內容作為示範 (例如：半透明標籤或圖案)
+    pg.fill(255, 255, 0, 150); // 黃色半透明
+    pg.noStroke();
+    pg.rect(20, 20, 150, 50, 10);
+    pg.fill(0);
+    pg.textAlign(CENTER, CENTER);
+    pg.text("LIVE CAMERA", 95, 45);
+  }
+
   // 計算影像寬高為畫布的 60%
   let w = width * 0.6;
   let h = height * 0.6;
@@ -20,5 +34,10 @@ function draw() {
   scale(-1, 1);
   // 在翻轉後的座標系中，將影像繪製在中心 (起點為 -w/2, -h/2)
   image(capture, -w / 2, -h / 2, w, h);
+  
+  // 如果 pg 已建立，則將其繪製在視訊畫面的上方
+  if (pg) {
+    image(pg, -w / 2, -h / 2, w, h);
+  }
   pop();
 }
